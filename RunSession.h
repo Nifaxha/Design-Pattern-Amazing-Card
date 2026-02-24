43a6d39bfa4acf5fa908dd1ad0b5962782ae633a
Nifaxha <harizriza30@gmail.com>
2026-02-25 04:21:58 +0700

    Create RunSession.h
    -Pmebuatan RunSession.h

---
+++ RunSession.h
@@ -0,0 +1,17 @@
+#ifndef RUNSESSION_H
+#define RUNSESSION_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+class RunSession {
+private:
+    int currentScore;
+    int targetScore;
+    int handsRemaining;
+    int discardsRemaining;
+    int currentRound;
+};
+
+#endif
