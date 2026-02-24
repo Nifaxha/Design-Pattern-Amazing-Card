3da94cc99a2dfc6f1e2b7e88ce7d0bea63353a54
Nifaxha <harizriza30@gmail.com>
2026-02-25 04:25:22 +0700

    Update RunSessions.h
    - Penambahan acces modifier public untuk dapat diakses dari mana saja

--- RunSession.h
+++ RunSession.h
@@ -12,6 +12,22 @@ private:
     int handsRemaining;
     int discardsRemaining;
     int currentRound;
+
+public:
+    RunSession();
+    
+    // Fungsi utama untuk menjalankan loop permainan
+    void startRound();
+    bool isRoundOver();
+    bool isGameOver();
+    
+    // Getters & Setters sederhana
+    void addScore(int points);
+    void useHand();
+    void useDiscard();
+    
+    // Display status ke terminal VS Code
+    void displayStatus();
 };
 
 #endif
