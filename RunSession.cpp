a36babe85ec1c053ebcf7e4211ef4aef67dfb39d
Nifaxha <harizriza30@gmail.com>
2026-02-25 10:52:40 +0700

    RunSession
    -Pembuatan RunSession.cp
    -Pemberian Nama judul dan nama aset

---
+++ RunSession.cpp
@@ -0,0 +1,41 @@
+#include "RunSession.h"
+using namespace std;
+
+RunSession::RunSession() {
+    currentScore = 0;
+    targetScore = 300; // Target awal ronde 1
+    handsRemaining = 4;
+    discardsRemaining = 3;
+    currentRound = 1;
+}
+
+void RunSession::displayStatus() {
+    cout << "\n==============================\n";
+    cout << "        MAGIC CARD CONSOLE       \n";
+    cout << "==============================\n";
+    cout << " Round: " << currentRound << "\n";
+    cout << " Score: " << currentScore << " / " << targetScore << "\n";
+    cout << " Hands: " << handsRemaining << " | Discards: " << discardsRemaining << "\n";
+    cout << "==============================\n";
+}
+
+void RunSession::addScore(int points) {
+    currentScore += points;
+    cout << ">> Kamu mendapatkan " << points << " Aset!\n";
+}
+
+void RunSession::useHand() {
+    if (handsRemaining > 0) handsRemaining--;
+}
+
+void RunSession::useDiscard() {
+    if (discardsRemaining > 0) discardsRemaining--;
+}
+
+bool RunSession::isRoundOver() {
+    return (currentScore >= targetScore);
+}
+
+bool RunSession::isGameOver() {
+    return (handsRemaining <= 0 && currentScore < targetScore);
+}
